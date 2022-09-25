4641a0df38e49c4f43f5bbffa5e871764403a75b
Chronos093 <gois.carlosalberto93@gmail.com>
2022-09-25 20:48:30 -0300

    Finalizado projeto Fundo Imobiliário

--- fundo_imobiliario/main.py
+++ fundo_imobiliario/main.py
@@ -60,7 +60,7 @@ conteudo = ["<!DOCTYPE html>", \
 "<head>", \
 "    <meta charset='utf-8'>", \
 "    <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no'>", \
-"    <title>fii</title>", \
+"    <title>Dash - FII</title>", \
 "    <link rel='stylesheet' href='assets/bootstrap/css/bootstrap.min.css'>", \
 "    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Alfa+Slab+One'>", \
 "    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Asset'>", \
