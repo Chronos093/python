88a237a1b7006d7ca2c0c73eec430d77ab30a4e0
Chronos093 <gois.carlosalberto93@gmail.com>
2022-10-14 18:39:39 -0300

    Agora o script do fii envia o index.html para o server nginx

--- fundo_imobiliario/fii_main.py
+++ fundo_imobiliario/fii_main.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import date
 import webbrowser
+import os
 
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
 
@@ -139,4 +140,8 @@ for linhas in conteudo:
 
 arq.close()
 
-webbrowser.open('index.html')
+# ENVIA O HTML PARA O SERVIDOR DE TESTES DO NGINX
+os.system('scp index.html ubuntu@192.168.1.110:/home/ubuntu/nginx/www/fii-monitor')
+
+# ABRE A PÁGINA WEB QUANDO TERMINA DE RODAR O SCRIPT
+#webbrowser.open('index.html')
