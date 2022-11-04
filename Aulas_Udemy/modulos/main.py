ceb9aac5949e59f0f6b9a1f55921b6432d434f7c
Carlos Alberto de Gois <70582239+Chronos093@users.noreply.github.com>
2022-11-04 21:54:32 +0000

    Aula de módulos

---
+++ Aulas_Udemy/modulos/main.py
@@ -0,0 +1,16 @@
+from funcoes import somar, multi, find_index
+from itens.cadastro import cliente
+
+somar()
+multi()
+cliente()
+
+print()
+
+###################################
+
+list1 = ['a', 'b', 'c', 'd', 'e']
+
+var1 = find_index(list1, 'b')
+
+print(var1)
